58e49f0936cea7264f651a0694c4fc82c60e8229
Bruno Roy <brusi_roy@hotmail.com>
2018-07-28 19:57:53 +0000

    Add basic client implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+extern crate grpcio;
+extern crate protobuf;
+extern crate futures;
+
+#[path="../kontakto-common/protos/kontakto.rs"]
+mod kontakto;
+#[path="../kontakto-common/protos/kontakto_grpc.rs"]
+mod kontakto_grpc;
+
+use std::sync::Arc;
+
+use grpcio::{ChannelBuilder, EnvBuilder};
+
+use kontakto::{CreateContactRequest,
+               UpdateContactRequest,
+               DeleteContactRequest,
+               GetAllContactsRequest};
+
+use kontakto_grpc::KontaktoClient;
+
+
 fn main() {
-    println!("Hello, world!");
+    let env = Arc::new(EnvBuilder::new().build());
+    let ch = ChannelBuilder::new(env).connect("localhost:50051");
+    let client = KontaktoClient::new(ch);
+
+    // Create a `CreateContactRequest`
+    let req = CreateContactRequest::new();
+    client.create_contact(&req).expect("rpc");
+
+    // Create a `UpdateContactRequest`
+    let req = UpdateContactRequest::new();
+    client.update_contact(&req).expect("rpc");
+
+    // Create a `DeleteContactRequest`
+    let req = DeleteContactRequest::new();
+    client.delete_contact(&req).expect("rpc");
+
+    // Create a `GetAllContactsRequest`
+    let req = GetAllContactsRequest::new();
+    client.get_all_contacts(&req).expect("rpc");
 }
